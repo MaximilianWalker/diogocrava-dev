4ec7c50b02da27523bab3d7b8a88156d6865b4bc
Maximilian Walker <diogocrava50@gmail.com>
2023-02-20 01:14:54 +0000

    Icons, Animation Library and Glitch Button Animation Fix

--- src/components/Buttons/glitchyButton.js
+++ src/components/Buttons/glitchyButton.js
@@ -3,7 +3,7 @@ import styles from './glitchyButton.module.css';
 const GlitchyButton = ({ children, ...props }) => {
 
     return (
-        <button className={styles["btn-glitch"]}>
+        <button className={styles["btn-glitch"]} {...props}>
             <span className={styles["text"]}>// {children}</span><span className={styles["text-decoration"]}>_</span><span className={styles["decoration"]}>&rArr;</span>
         </button>
     );
