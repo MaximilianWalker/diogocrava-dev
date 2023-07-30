24110b5f98ed4b8912d2c38ed85dc1938c146826
Maximilian Walker <diogocrava50@gmail.com>
2023-07-30 19:18:45 +0100

    Terminal Changes

--- src/components/TypeIt/terminal.js
+++ src/components/TypeIt/terminal.js
@@ -1,6 +1,6 @@
 'use client';
 
-import { useEffect, useState } from "react";
+import { useEffect, useState, useRef } from "react";
 
 import Prism from "prismjs";
 
@@ -15,9 +15,7 @@ import "./darkPlusPrismTheme.css";
 import TypeIt from "typeit-react";
 import styles from './terminal.module.css';
 
-const initialMessage = String.raw`
-<pre>
-BIOS:
+const initialMessage = String.raw`BIOS:
   POST: All devices are functional
   Booting from Hard Drive...
   GRUB Loading kernel...
@@ -30,12 +28,23 @@ Linux Boot:
 [    0.000000] Initializing cgroup subsys cpu
 [    0.000000] Linux version 5.4.0-66-generic (buildd@lcy01-amd64-029) (gcc version 7.5.0 (Ubuntu 7.5.0-3ubuntu1~18.04)) #74-Ubuntu SMP
 
-... (a lot of hardware and kernel initialization messages) ...
+[    0.000000] Command line: BOOT_IMAGE=/boot/vmlinuz-5.4.0-66-generic root=UUID=0a3407de-e4a6-4b17-97c1-1a7dc718c3db ro quiet splash
+[    0.000000] KERNEL supported cpus:
+[    0.000000] MDS: Vulnerable: Clear CPU buffers attempted, no microcode
+[    0.000000] x86/fpu: Supporting XSAVE feature 0x001: 'x87 floating point registers'
+[    0.000000] x86/fpu: Supporting XSAVE feature 0x002: 'SSE registers'
 
 [    5.618647] systemd[1]: systemd 237 running in system mode. (+PAM +AUDIT +SELINUX +IMA +APPARMOR +SMACK +SYSVINIT +UTMP +LIBCRYPTSETUP +GCRYPT +GNUTLS +ACL +XZ +LZ4 +SECCOMP +BLKID +ELFUTILS +KMOD -IDN2 +IDN -PCRE2 default-hierarchy=hybrid)
 [    5.635746] systemd[1]: Detected architecture x86-64.
 
-... (systemd service startup messages) ...
+[    5.65897] systemd[1]: Set hostname to <ubuntu>.
+[    5.87500] systemd[1]: Listening on udev Control Socket.
+[    5.88554] systemd[1]: Started Dispatch Password Requests to Console Directory Watch.
+
+[    7.11234] systemd[1]: Starting Network Service...
+[    7.31384] systemd[1]: Started Network Service.
+[    8.04193] systemd[1]: Starting Network Manager...
+[    8.09334] systemd[1]: Started Network Manager.
 
 [   10.06431] systemd[1]: Reached target Graphical Interface.
 [   10.08185] systemd[1]: Starting Update UTMP about System Runlevel Changes...
@@ -44,58 +53,68 @@ Linux Boot:
 
 ====================================================================================================
 
-/ $$$$$$$  /$$                                      / $$$$$$                                        
-| $$__  $$|__/                                     / $$__  $$                                       
+/ $$$$$$$  /$$                                      / $$$$$$
+| $$__  $$|__/                                     / $$__  $$
 | $$  \ $$ /$$  /$$$$$$   /$$$$$$   /$$$$$$       | $$  \__/  /$$$$$$  /$$$$$$  /$$    /$$ /$$$$$$ 
 | $$  | $$| $$ /$$__  $$ /$$__  $$ /$$__  $$      | $$       /$$__  $$|____  $$|  $$  /$$/|____  $$
 | $$  | $$| $$| $$  \ $$| $$  \ $$| $$  \ $$      | $$      | $$  \__/ /$$$$$$$ \  $$/$$/  /$$$$$$$
 | $$  | $$| $$| $$  | $$| $$  | $$| $$  | $$      | $$    $$| $$      /$$__  $$  \  $$$/  /$$__  $$
 | $$$$$$$/| $$|  $$$$$$/|  $$$$$$$|  $$$$$$/      |  $$$$$$/| $$     |  $$$$$$$   \  $/  |  $$$$$$$
 |_______/ |__/ \______/  \____  $$ \______/        \______/ |__/      \_______/    \_/    \_______/
-                         /$$  \ $$                                                                 
-                        |  $$$$$$/                                                                 
-                         \______/                                                                  
-                                     / $$$$$$   /$$$$$$                                      
-                                    / $$__  $$ /$$__  $$                                     
-                                    | $$  \ $$| $$  \__/                                     
-                                    | $$  | $$|  $$$$$$                                      
-                                    | $$  | $$ \____  $$                                     
-                                    | $$  | $$ /$$  \ $$                                     
-                                    |  $$$$$$/|  $$$$$$/                                     
-                                    \______/  \______/   
+                         /$$  \ $$
+                        |  $$$$$$/
+                         \______/
+                                     / $$$$$$   /$$$$$$
+                                    / $$__  $$ /$$__  $$
+                                    | $$  \ $$| $$  \__/
+                                    | $$  | $$|  $$$$$$
+                                    | $$  | $$ \____  $$
+                                    | $$  | $$ /$$  \ $$
+                                    |  $$$$$$/|  $$$$$$/
+                                    \______/  \______/
 
-====================================================================================================
-</pre>
-`;
+====================================================================================================`;
 
-const messages = initialMessage.split('\n');
-for(let m of messages) console.log(m);
+const messages = initialMessage.split('\r\n');
+for(let i = 0; i < messages.length; i++) {
+    const message = messages[i];
+    if(message.length > 100){
+        messages[i] = message.substring(0, 100);
+        messages.splice(i + 1, 0, message.substring(100));
+    }
+}
 
-export default function Terminal({  }) {
+export default function Terminal({ background = false }) {
+    const tabRef = useRef();
     const [instance, setInstance] = useState();
     return (
-        <div className={styles.terminalContainer}>
-            <TypeIt
-                className={styles.terminal}
-                as="pre"
-                options={{
-                    cursorChar: "_",
-                    speed: 1,
-                    nextStringDelay: 0,
-                    // loop: true,
-                    // lifeLike: true,
-                    // deleteSpeed: 100,
-                    html: true
-                }}
-                getBeforeInit={(instance) => {
-                    messages.forEach((m) => {
-                        instance.type(m);
-                        instance.break();
-                    })
-                    setInstance(instance);
-                    return instance;
-                }}
-            />
+        <div className={styles.container}>
+            <div ref={tabRef} className={styles.tab}>
+                <button>x</button>
+            </div>
+            <div className={styles.terminalContainer}>
+                <TypeIt
+                    className={styles.terminal}
+                    as="pre"
+                    options={{
+                        cursorChar: "_",
+                        speed: 1,
+                        nextStringDelay: 0,
+                        // loop: true,
+                        // lifeLike: true,
+                        // deleteSpeed: 100,
+                        // html: true
+                    }}
+                    getBeforeInit={(instance) => {
+                        messages.forEach((m) => {
+                            instance.type(m);
+                            instance.break();
+                        })
+                        setInstance(instance);
+                        return instance;
+                    }}
+                />
+            </div>
         </div>
     );
 }
