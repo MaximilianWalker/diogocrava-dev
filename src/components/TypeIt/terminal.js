bd1140828c1f65dbde93edc56d42e46f4ce168fb
Maximilian Walker <diogocrava50@gmail.com>
2023-08-02 13:36:54 +0100

    Terminal Bug Fixes

--- src/components/TypeIt/terminal.js
+++ src/components/TypeIt/terminal.js
@@ -15,15 +15,15 @@ import "./darkPlusPrismTheme.css";
 import TypeIt from "typeit-react";
 import styles from './terminal.module.css';
 
-function parseText(text) {
+function splitText(text) {
     const lines = text.split(/\r?\n/);
-    for (let i = 0; i < lines.length; i++) {
-        const line = lines[i];
-        if (line.length > 100) {
-            lines[i] = line.substring(0, 100);
-            lines.splice(i + 1, 0, line.substring(100));
-        }
-    }
+    // for (let i = 0; i < lines.length; i++) {
+    //     const line = lines[i];
+    //     if (line.length > 100) {
+    //         lines[i] = line.substring(0, 100);
+    //         lines.splice(i + 1, 0, line.substring(100));
+    //     }
+    // }
     return lines;
 }
 
@@ -44,13 +44,13 @@ export default function Terminal({ background = false }) {
     const getLinuxStartup = async () => {
         const response = await fetch('/texts/linux_startup.txt');
         const text = await response.text();
-        setLinuxStartup(parseText(text));
+        setLinuxStartup(splitText(text));
     }
 
     const getOsMessage = async () => {
         const response = await fetch('/texts/os.txt');
         const text = await response.text();
-        setOsMessage(parseText(text));
+        setOsMessage(splitText(text));
     }
 
     const onMouseDown = (e) => {
