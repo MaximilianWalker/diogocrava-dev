b10a274fdd50e5b4cbfc65e7fae346885276482e
Maximilian Walker <diogocrava50@gmail.com>
2024-02-13 04:35:19 +0000

    Particles Animation - Replacing TSParticles

--- src/utils/mathUtils.js
+++ src/utils/mathUtils.js
@@ -1,5 +1,14 @@
-export const distance = (x1, y1, x2, y2) => {
-    const xDist = x2 - x1;
-    const yDist = y2 - y1;
+export const distance = (p1, p2) => {
+    const xDist = p2.x - p1.x;
+    const yDist = p2.y - p1.y;
     return Math.sqrt(Math.pow(xDist, 2) + Math.pow(yDist, 2));
 };
+
+// export const areaOfTriangle = (p1, p2, p3) => {
+//     return 0.5 * Math.abs(p1.x * (p2.y - p3.y) + p2.x * (p3.y - p1.y) + p3.x * (p1.y - p2.y));
+// };
+
+export const areaOfTriangle = (side1, side2, side3) => {
+    const s = (side1 + side2 + side3) / 2;
+    return Math.sqrt(s * (s - side1) * (s - side2) * (s - side3));
+}
