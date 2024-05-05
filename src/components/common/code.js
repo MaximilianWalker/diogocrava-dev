d42d69b9f51719ee285c86bed4d6f47615f88e5f
Maximilian Walker <diogocrava50@gmail.com>
2024-05-05 03:59:22 +0100

    Code Component Clean

--- src/components/common/code.js
+++ src/components/common/code.js
@@ -3,134 +3,30 @@
 
 import { useEffect, useMemo } from "react";
 import PropTypes from 'prop-types';
-// import Prism from "prismjs";
 import SyntaxHighlighter from 'react-syntax-highlighter';
-
-// import { SyntaxHighlighter } from 'react-syntax-highlighter';
 import { lioshi } from 'react-syntax-highlighter/dist/esm/styles/hljs';
-
-
 import "./code.css";
 
-// import "prismjs/components/prism-javascript";
-// import "prismjs/components/prism-jsx";
-// import "prismjs/components/prism-json";
-// import "prismjs/components/prism-xml-doc";
-// import "prismjs/components/prism-python";
-// import 'prismjs/plugins/line-numbers/prism-line-numbers.js'
-// import 'prismjs/plugins/line-numbers/prism-line-numbers.css'
+// Github
+// https://github.com/react-syntax-highlighter/react-syntax-highlighter
 
-// function Code({ className, code, language, showLineNumbers }) {
-//     useEffect(() => {
-//         Prism.highlightAll();
-//     }, [code, language]);
-//     return (
-//         <div className="code__container">
-//             <pre className={`line-numbers language-${language} ${className ?? ''}`}>
-//                 <code>
-//                     {code}
-//                 </code>
-//             </pre>
-//         </div>
-//     );
-// };
-
-// function Code({ className, code, language, showLineNumbers }) {
-//     // const highlightedCode = useMemo(() => Prism.highlight(code, Prism.languages[language], language), [code, language]);
-//     // useEffect(() => {
-//     //     Prism.highlightAll();
-//     // }, [code, language]);
-//     // console.log(highlightedCode);
-//     return (
-//         <div className="code__container">
-//             {/* <SyntaxHighlighter language={language} showLineNumbers>
-//                 {code}
-//             </SyntaxHighlighter> */}
-//             <Markdown
-//                 children={code}
-//                 components={{
-//                     code({ children, className, node, ...rest }) {
-//                         return (
-//                             <SyntaxHighlighter
-//                                 {...rest}
-//                                 // PreTag="div"
-//                                 // children={String(children).replace(/\n$/, '')}
-//                                 language={language}
-//                                 style={twilight}
-//                             />
-//                         );
-//                     }
-//                 }}
-//             />
-//         </div>
-//     );
-// };
+// Styles
+// https://react-syntax-highlighter.github.io/react-syntax-highlighter/demo/
 
 function Code({ className, code, language, showLineNumbers }) {
-    // const highlightedCode = useMemo(() => Prism.highlight(code, Prism.languages[language], language), [code, language]);
-    // useEffect(() => {
-    //     Prism.highlightAll();
-    // }, [code, language]);
-    // console.log(highlightedCode);
     return (
-        <div className="code__container">
-            <SyntaxHighlighter
-                language={language}
-                style={lioshi}
-                PreTag="div"
-                showLineNumbers
-                useInlineStyles
-            >
-                {code}
-            </SyntaxHighlighter>
-        </div>
+        <SyntaxHighlighter
+            className={className}
+            language={language}
+            style={lioshi}
+            // PreTag="div"
+            showLineNumbers={showLineNumbers}
+            // useInlineStyles
+        >
+            {code}
+        </SyntaxHighlighter>
     );
 };
-
-// function Code({ className, code, language, showLineNumbers }) {
-//     const highlightedCode = useMemo(() => Prism.highlight(code, Prism.languages[language], language), [code, language]);
-//     // useEffect(() => {
-//     //     Prism.highlightAll();
-//     // }, [code, language]);
-//     // console.log(highlightedCode);
-//     return (
-//         <div className="code__container">
-//             {/* <SyntaxHighlighter language={language} showLineNumbers>
-//                 {code}
-//             </SyntaxHighlighter> */}
-//             <Markdown
-//                 children={highlightedCode}
-//             />
-//         </div>
-//     );
-// };
-
-// function Code({ className, code, language, showLineNumbers }) {
-//     // const highlightedCode = useMemo(() => Prism.highlight(code, Prism.languages[language], language), [code, language]);
-//     // useEffect(() => {
-//     //     Prism.highlightAll();
-//     // }, [code, language]);
-//     // console.log(highlightedCode);
-//     const highlightedCode = useMemo(() => {
-//         const result = unified()
-//             .use(remarkParse)
-//             .use(remarkRehype, { allowDangerousHtml: true })
-//             .use(rehypeSanitize)
-//             .use(rehypeStringify)
-//             .processSync(code);
-//         return result.toString();
-//     })
-//     return (
-//         <div className="code__container">
-//             {/* <SyntaxHighlighter language={language} showLineNumbers>
-//                 {code}
-//             </SyntaxHighlighter> */}
-//             <Markdown
-//                 children={highlightedCode}
-//             />
-//         </div>
-//     );
-// };
 
 Code.propTypes = {
     className: PropTypes.string,
